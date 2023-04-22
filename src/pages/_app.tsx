18cc6f841ae6640e46c7be0ee15334e9428cd74d
lengyue <lengyue@lengyue.me>
2023-04-21 21:50:44 -0400

    更新文档

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <title>弹幕播放器</title>
-    </Head>
-    
-    <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <Head>
+        <title>弹幕播放器</title>
+      </Head>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
